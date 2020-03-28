554de3d03644d46857c149d77c09df0759c13530
Vrinda Singh <vri.s.ch9@gmail.com>
2020-03-29 02:27:44 +0530

    init: main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    # Inputs
+    inputs = [
+    [1,2,3,4,5,6,7],
+    [1,10,5,21,4],
+    [1,10,5,21,4,1]]
+
+    for arr in inputs:
+        # TODO: partion = Partionable(arr)
+        # which should expose canPartion method
+        if True == True:
+            print ("Partionale")
+        else:
+            print ("Non Partionable")
